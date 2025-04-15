49a34fac23bea3d23f17142645f6d0179b7fa675
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-15 22:18:13 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Humbl Cookies",
+  description: "Лучшие печеньки в Санкт-Петербурге",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
