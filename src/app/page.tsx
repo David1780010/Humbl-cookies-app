b45f38141201d57cf7d63f0d3efd0b861b9e31ae
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 08:37:24 +0300

    Добавил изображение в верхнюю часть страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import Image from "next/image";
 import { initTelegramApp } from '@/utils/telegram';
 
 export default function Home() {
@@ -10,6 +11,13 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-white">
+      <div className="w-full">
+        <img 
+          src="https://i.postimg.cc/nhZJZcSy/detalis.png" 
+          alt="Details"
+          className="w-full"
+        />
+      </div>
     </div>
   );
 }
