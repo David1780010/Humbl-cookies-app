4af18d11c18cc1adf53ae11ca6bdd8019bd83c4b
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 11:27:40 +0300

    Сдвинул изображение вправо и вверх

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,18 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white">
-      <div className="w-full">
+    <div className="min-h-screen bg-white overflow-hidden">
+      <div className="w-full" style={{ marginTop: '-20px', marginRight: '-15px', position: 'relative' }}>
         <img 
           src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
           alt="Details"
           className="w-full h-auto"
-          style={{ maxHeight: '400px', objectFit: 'cover' }}
+          style={{ 
+            maxHeight: '400px', 
+            objectFit: 'cover',
+            transform: 'scale(1.1)',
+            transformOrigin: 'top right'
+          }}
         />
       </div>
     </div>
