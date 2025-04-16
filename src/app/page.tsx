1aeaa75a89aee12fcfa2d8051441c5a6a4129ee3
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 09:25:20 +0300

    Изменил способ позиционирования изображения

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,14 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-white p-4">
-      <div className="max-w-md mx-auto mt-4">
-        <img 
-          src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
-          alt="Details"
-          className="max-w-full h-auto max-h-[300px] mx-auto rounded-lg -ml-4"
-          style={{ display: 'block' }}
-        />
+      <div className="relative max-w-md mx-auto">
+        <div className="absolute left-[-20px] top-[-10px] w-[calc(100%+40px)]">
+          <img 
+            src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
+            alt="Details"
+            className="w-full h-auto max-h-[300px] object-contain"
+          />
+        </div>
       </div>
     </div>
   );
