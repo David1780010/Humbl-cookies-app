2a5aebb4c6ac6bad2bf2a4388ffd005c280c1811
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 09:03:39 +0300

    Заменил Next.js Image на HTML img тег

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from 'react';
 import { initTelegramApp } from '@/utils/telegram';
-import Image from 'next/image';
 
 export default function Home() {
   useEffect(() => {
@@ -13,13 +12,10 @@ export default function Home() {
     <div className="min-h-screen bg-white p-4">
       <div className="max-w-2xl mx-auto">
         <div className="w-full relative aspect-[3/2]">
-          <Image 
+          <img 
             src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
             alt="Details"
-            fill
-            style={{ objectFit: 'cover' }}
-            className="rounded-lg"
-            priority
+            className="w-full h-full object-cover rounded-lg"
           />
         </div>
       </div>
