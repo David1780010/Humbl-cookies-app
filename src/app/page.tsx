890d4ac26e38e4a2713a53be7c70d61ce265489a
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 09:15:57 +0300

    Испмравил позиционирование изображения

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white p-4 flex flex-col items-center">
-      <div className="w-full max-w-md mt-8 flex justify-center">
-        <div className="w-full flex justify-center">
+    <div className="min-h-screen bg-white p-4">
+      <div className="max-w-md mx-auto mt-8">
+        <div className="w-full relative">
           <img 
             src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
             alt="Details"
-            className="max-w-full max-h-[300px] object-contain rounded-lg"
+            className="w-full max-h-[300px] object-contain rounded-lg"
           />
         </div>
       </div>
