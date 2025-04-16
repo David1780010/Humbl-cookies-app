97ce01a2e73fd4914d803b03885e4ce4b25f8525
ProGramiiSt <ps5plus2608@gmail.com>
2025-04-16 09:18:36 +0300

    Исправил центрирование изображения

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,12 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-white p-4">
-      <div className="max-w-md mx-auto mt-8">
-        <div className="w-full relative">
-          <img 
-            src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
-            alt="Details"
-            className="w-full max-h-[300px] object-contain rounded-lg"
-          />
-        </div>
+      <div className="max-w-md mx-auto mt-8 text-center">
+        <img 
+          src="https://raw.githubusercontent.com/David1780010/Humbl-cookies-app/9d7381ab44330bb52318589923c85d38c72cb185/detalis.png"
+          alt="Details"
+          className="inline-block max-h-[300px] w-auto object-contain rounded-lg"
+        />
       </div>
     </div>
   );
